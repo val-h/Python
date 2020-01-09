ec044a0a522bc7a5e1c548cf1e12148711cc9bc6
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-01-09 00:44:22 +0000

    Password program

---
+++ Loops/Password.py
@@ -0,0 +1,9 @@
+usn = input('Name: ')
+psw = input('Password: ')
+
+passInput = input('Enter your password: ')
+while passInput != psw:
+    print('Wrong password')
+    passInput = input('Enter your password again: ')
+
+print(f'Welcome {usn}!')
