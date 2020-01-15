e7d7e3d6d528f0a0cd57f814a71eaefbda61c4fb
Valentin <valentin97mirchev@gmail.com>
2020-01-15 02:54:22 +0000

    2 hours well spent?

--- Ex Loops/Coins.py
+++ Ex Loops/Coins.py
@@ -1,24 +1,26 @@
+from decimal import *
+getcontext().prec = 2
 money = float(input('Amount of money: '))
-dec = money
-coins = 0
-while money > 0.00:
-    if money >= 2:
-        money -= 2
-    elif money >= 1:
-        money -= 1
-    elif money >= 0.5:
-        money -= 0.5
-    elif money >= 0.2:
-        money -= 0.2
-    elif money >= 0.1:
-        money -= 0.1
-    elif money >= 0.05:
-        money -= 0.05
-    elif money >= 0.02:
-        money -= 0.02
-    elif money >= 0.01:
-        money -= 0.01
-    coins += 1
-    #todo finish this mess - old problem with floating point numbers : /
-    print(money)
-print(coins)
+p = int(money * 100)
+numCoins = 0
+while p > 0:
+    if p >= 200:
+        p -= 200
+    elif p >= 100:
+        p -= 100
+    elif p >= 50:
+        p -= 50
+    elif p >= 20:
+        p -= 20
+    elif p >= 10:
+        p -= 10
+    elif p >= 5:
+        p -= 5
+    elif p >= 2:
+        p -= 2
+    elif p >= 1:
+        p -= 1
+    numCoins += 1
+    print(p)
+print(f'Number of coins: {numCoins}')
+# at long long last... i could have done this alot faster : / ...
--- Ex Loops/Exam prep.py
+++ Ex Loops/Exam prep.py
@@ -1,10 +1,23 @@
-badGrades = int(input'Bad grades: ')
+badGrades = int(input('Bad grades: '))
 grdCnt = 0
 exercName = None
+totalGrade = 0
+i = 0
+lastExr = None
 while grdCnt < badGrades:
     exercName = input('Exercise name: ')
+    if exercName == 'Enough':
+        break
     grade = float(input('Grade: '))
-    if grade < 4:
+    totalGrade += grade
+    if grade <= 4:
         grdCnt+=1
-if grdCnt >= badGrades:
-    pass #todo finish this
+    i += 1
+    lastExr = exercName
+totalGrade /= i
+if grdCnt == badGrades:
+    print(f'You need a break, {grdCnt} poor grades.')
+elif exercName == 'Enough':
+    print(f'Average score: {totalGrade:.2f}')
+    print(f'Number of problems: {i}')
+    print(f'Last problem: {lastExr}')
