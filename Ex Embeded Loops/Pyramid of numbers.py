251b36953c3dc0728999829d51ce73c26a0afaa9
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-01-22 04:27:28 +0000

    first exercise from ex emb loops

---
+++ Ex Embeded Loops/Pyramid of numbers.py
@@ -0,0 +1,15 @@
+# i don't really know how this works
+n = int(input('Enter a value for n: '))
+stop = False
+cnt = 1
+currentLine = ''
+for i in range(1, n + 1):
+    for q in range(0, i):
+        print(str(cnt) + ' ', end='')
+        if cnt == n:
+            stop = True
+            break
+        cnt+=1
+    print()
+    if stop == True:
+        break
