3453029cd0ea1b1350a0ce03cfd105a26202f7b2
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-01-25 23:48:09 +0000

    Simple one

---
+++ Exam exercises/Bachelor Party.py
@@ -0,0 +1,25 @@
+#   User input
+speaker = int(input('Cost for speaker: '))
+
+#   Variables
+guestSum = 0
+totalGuests = 0
+usrInp = None
+
+#   Main Loop
+while True:
+    usrInp = input('Enter the number of people in the group: ')
+    if usrInp == 'The restaurant is full':
+        break
+    groupCount = int(usrInp)
+    if groupCount < 5:
+        guestSum += groupCount * 100
+    else:
+        guestSum += groupCount * 70
+    totalGuests += groupCount
+
+#   Output
+if guestSum >= speaker:
+    print(f'You have {totalGuests} guests and {guestSum - speaker} leve left.')
+else:
+    print(f'You have {totalGuests} guests and {guestSum} leva income, but no singer.')
