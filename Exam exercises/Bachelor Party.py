d9a7fb030c6dedd7e2b0b78db156098b0c8850d1
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-01-25 23:51:36 +0000

    Compressing the code

--- Exam exercises/Bachelor Party.py
+++ Exam exercises/Bachelor Party.py
@@ -6,20 +6,15 @@ guestSum = 0
 totalGuests = 0
 usrInp = None
 
-#   Main Loop
+#   Loop
 while True:
     usrInp = input('Enter the number of people in the group: ')
-    if usrInp == 'The restaurant is full':
-        break
+    if usrInp == 'The restaurant is full':  break
     groupCount = int(usrInp)
-    if groupCount < 5:
-        guestSum += groupCount * 100
-    else:
-        guestSum += groupCount * 70
+    if groupCount < 5:  guestSum += groupCount * 100
+    else:   guestSum += groupCount * 70
     totalGuests += groupCount
 
 #   Output
-if guestSum >= speaker:
-    print(f'You have {totalGuests} guests and {guestSum - speaker} leve left.')
-else:
-    print(f'You have {totalGuests} guests and {guestSum} leva income, but no singer.')
+if guestSum >= speaker:   print(f'You have {totalGuests} guests and {guestSum - speaker} leve left.')
+else:   print(f'You have {totalGuests} guests and {guestSum} leva income, but no singer.')
