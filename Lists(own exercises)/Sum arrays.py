175ca9d3e04546cf5a39a260cbeac43b7a2df810
Valentin <valentin97mirchev@gmail.com>
2020-01-30 14:25:45 +0000

    Must check this out later!

--- Lists(own exercises)/Sum arrays.py
+++ Lists(own exercises)/Sum arrays.py
@@ -2,5 +2,16 @@ firstListOfNums = input('First list of nums: ')
 secondListOfNums = input('Second list of nums: ')
 arr1 = [int(x) for x in firstListOfNums.split()]
 arr2 = [int(x) for x in secondListOfNums.split()]
+longArr = []
+shortArr = []
 
-# TODO finish please : <
+if len(arr1) >= len(arr2):
+    longArr = arr1
+    shortArr = arr2
+else:
+    longArr = arr2
+    shortArr = arr1
+
+for i in range(0, len(longArr)):
+    print(longArr[i] + shortArr[i % len(shortArr)]) # Actually works need to check it out later
+    
