524ce5c1673bf1c1bb315e1290601c76c3788a4d
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-02-06 10:42:35 +0000

    I'm way too slow

--- Lists more exercises/Max Sequence of Equal Elements.py
+++ Lists more exercises/Max Sequence of Equal Elements.py
@@ -1,7 +1,14 @@
 line_of_numbers = input('Line of numbers: ')
 arrNums = [int(x) for x in line_of_numbers.split()]
 longestEqualSequence = ''
-start = 0
+tempSeq = f'{arrNums[0]} '
 
-for i in range(0, len(arrNums)):
-    pass    # Finish this
+for i in range(1, len(arrNums)):        # Looping through arrNums starting at index 1
+    if arrNums[i] == arrNums[i - 1]:    # If arrNums at index i == arrNums at the previou index
+        tempSeq += f'{arrNums[i]} '     
+    else:                               # If not tempSeq gets reset
+        tempSeq = f'{arrNums[i]} '
+    if len(tempSeq) > len(longestEqualSequence):    # If tempSeq is larger(by lenght)
+        longestEqualSequence = tempSeq
+# Quite the simple program yet it took me way too long and at first i tried a solution with 3 loops 
+print(longestEqualSequence) # Then with 2... then i realized this is actually a no brainer...
