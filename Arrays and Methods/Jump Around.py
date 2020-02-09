72a724ba9f6a35c8da1ac972854dec28c171774e
Valentin Mirchev <valentin97mirchev@gmail.com>
2020-02-09 19:17:16 +0000

    final one

--- Arrays and Methods/Jump Around.py
+++ Arrays and Methods/Jump Around.py
@@ -1,1 +1,16 @@
-# Much code
+lineOfNums = input('Integers: ')
+nums = [int(x) for x in lineOfNums.split()]
+i = 0
+sum = 0
+
+while True:
+    sum += nums[i]
+    if len(nums) - i > nums[i]:
+        i += nums[i]
+    elif i - nums[i] >= 0:
+        i -= nums[i]
+    else:
+        break
+
+print(sum)
+6
