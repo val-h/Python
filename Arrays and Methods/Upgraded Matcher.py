fa61503a225188cde4070b629081de5160d5f734
Valentin <valentin97mirchev@gmail.com>
2020-02-09 16:59:00 +0000

    finished

--- Arrays and Methods/Upgraded Matcher.py
+++ Arrays and Methods/Upgraded Matcher.py
@@ -5,17 +5,25 @@ products = lineOfProducts.split()
 quantities = [int(x) for x in lineOfQuantities.split()]
 prices = [float(x) for x in lineOfPrices.split()]
 
-def ProductCost(product):
+def ProductCost(product, quantity):
     i = products.index(product)
-    if i < len(quantities): # Finish this
-        print('Yes')
+    tempCost = 0
+    if i < len(quantities):
+        if quantity <= quantities[i]:
+            tempCost = quantity * prices[i]
+            quantities[i] -= quantity
+            return f'{product} x {quantity} costs {tempCost:.2f}'
+        else:
+            return f'We do not have enough {product}'
     else:
-        print('No')
+        return f'We do not have enough {product}'
 
 while True:
     cmd = input('/> ')
     if cmd == 'done': break
-    if cmd in products:
-        ProductCost(cmd)
+    tProd, tQuan = cmd.split()
+    tQuan = int(tQuan)
+    if tProd in products:
+        print(ProductCost(tProd, tQuan))
     else:
         print(f'Error. Command {cmd} does not exist.')
